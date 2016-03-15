6cf95f3194717c0fd2c66d2056d4ae18eca9a202
Rob Cornish <rob@robcornishmusic.com>
2016-03-15 22:41:33 +1100

    updated robot and driver specs

--- spec/driver_spec.rb
+++ spec/driver_spec.rb
@@ -3,19 +3,21 @@ require_relative "../driver"
 
 describe Driver do
 
-  before :each do
-    @test_commands_1 = File.expand_path("../test_commands_1.txt", __FILE__)
-    @driver = Driver.new(@test_commands_1)
-  end
+  context "driver" do
+    subject { described_class.new(File.expand_path("../test_commands_1.txt", __FILE__)) }
 
-  it "is a driver" do
-    expect(@driver).to be_a(Driver)
-  end
+    it "is a driver" do
+      expect(subject).to be_a(Driver)
+    end
 
-  it "can read commands" do
-    expect(@driver.robot_report).to eq(["2, 2, NORTH"])
-  end
+    it "can read commands" do
+      expect(subject.robot_report).to eq(["2, 2, NORTH"])
+    end
 
-  
+    # it "ignores commands before a valid place command" do
+    #   subject.read_commands(File.expand_path("../test_commands_4.txt", __FILE__))
+    #   expect(subject.robot_report).to eq(["1, 2, EAST"])
+    # end
+  end
 
 end
